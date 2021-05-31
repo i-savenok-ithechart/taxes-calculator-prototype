95313e2b926de7db5bc32aba3c86e6616af81394
i-savenok-itechart <i.savenok.itechart@gmail.com>
2021-05-31 13:42:37 +0300

    django project init

---
+++ src/common/log.py
@@ -0,0 +1,12 @@
+import logging as _logging
+
+getLogger = _logging.getLogger
+basicConfig = _logging.basicConfig
+CRITICAL = _logging.CRITICAL
+FATAL = _logging.FATAL
+ERROR = _logging.ERROR
+WARNING = _logging.WARNING
+WARN = _logging.WARN
+INFO = _logging.INFO
+DEBUG = _logging.DEBUG
+NOTSET = _logging.NOTSET
